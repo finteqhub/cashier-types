e42073ad524756420e209584f992fabbe0e560f1
Serge Adamovich <sergey.adamovich@finteqhub.com>
2025-06-16 15:54:04 +0200

    Initialize project with types, config, and CI workflow

---
+++ src/index.ts
@@ -0,0 +1,67 @@
+export type FinteqHubCashierOptions = {
+  backdropStyleOptions?: Record<string, string>;
+  balance?: number; // todo: add support in cashier
+  containerStyleOptions?: Record<string, string>;
+  iframeUrl?: string;
+  initToken?: string; // todo: add support in cashier
+  locale?: string; // todo: add support in cashier
+  showClose?: boolean;
+  targetContainer?: HTMLElement;
+  theme?: string;
+};
+
+export type FinteqHubCashierEvent = {
+  type:
+    | "IFRAME_LOADED"
+    | "IFRAME_REMOVED"
+    | "MESSAGE_SENT"
+    | "WIDGET_CLOSED"
+    | "WIDGET_OPENED";
+  payload?: Record<string, unknown>;
+};
+
+export type FinteqHubCashierMessage = {
+  type: "SET_OPTIONS";
+  payload?: Record<string, unknown>;
+};
+
+export const VALID_OPTION_KEYS = [
+  "backdropStyleOptions",
+  "balance",
+  "containerStyleOptions",
+  "iframeUrl",
+  "initToken",
+  "locale",
+  "showClose",
+  "targetContainer",
+  "theme",
+] as const satisfies ReadonlyArray<keyof FinteqHubCashierOptions>;
+
+export const IFRAME_OPTIONS = [
+  "balance",
+  "initToken",
+  "locale",
+  "showClose",
+  "theme",
+] as const satisfies ReadonlyArray<keyof FinteqHubCashierOptions>;
+
+type AllOptionKeys = (typeof VALID_OPTION_KEYS)[number];
+type CheckOptionKeys = [
+  Exclude<keyof FinteqHubCashierOptions, AllOptionKeys>,
+] extends [never]
+  ? [Exclude<AllOptionKeys, keyof FinteqHubCashierOptions>] extends [never]
+    ? true
+    : never
+  : never;
+
+type IframeOptionKeys = (typeof IFRAME_OPTIONS)[number];
+type CheckIframeOptionKeys = [
+  Exclude<IframeOptionKeys, keyof FinteqHubCashierOptions>,
+] extends [never]
+  ? true
+  : never;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const _check: CheckOptionKeys = true;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const _checkIframe: CheckIframeOptionKeys = true;
