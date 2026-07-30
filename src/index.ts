2d1913c7ad9f48cbb7782f32fd7129cc0b0c98a3
Mikalai Fitkovich <mikalaifitkovich@pgw.dev>
2026-07-30 15:12:42 +0200

    updated theme types for new blue-orange theme

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ export type CashierBonus = {
 
 export type CashierLottery = CashierBonus;
 
-export type CashierTheme = "light" | "dark" | "blue" | "green";
+export type CashierTheme = "light" | "dark" | "blue" | "green" | "blue-orange";
 export type CashierTransactionType = "deposit" | "withdrawal";
 export type CashierMode = "full" | "oneClick";
 
